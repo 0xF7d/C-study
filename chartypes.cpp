3d81f9074af2f141572e4571648d7aaed74adafa
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2022-07-02 09:23:31 -0700

    Update chartypes.cpp

--- chartypes.cpp
+++ chartypes.cpp
@@ -73,46 +73,7 @@
     syntax: element type before brackets of size (int x_array[100];)
     sizeof to obtain total size in bytes
         #of elements: sizeof(x)/sizeof(y) (EX: 7)
-
- * ASCII:
-   		       Control codes Printable characters 
-	|-------------------------------------------------------------|
-	|	0d 0x Code |  0d 0x Char   | 0d 0x Char  | 0d 0x Char |
-	|-------------------------------------------------------------| 
-	|	0  0   NULL|  32  20 SPACE | 64 40   @   | 96  60  `  |
-	|	1  1   SOH |  33  21   !   | 65 41   A   | 97  61  a  | 
-	|	2  2   STX |  34  22   "   | 66 42   B   | 98  62  b  |  
-	|	3  3   ETX |  35  23   #   | 67 43   C   | 99  63  c  |  
-	|	4  4   EOT |  36  24   $   | 68 44   D   | 100 64  d  |  
-	|	5  5   ENQ |  37  25   %   | 69 45   E   | 101 65  e  |  
-	|	6  6   ACK |  38  26   &   | 70 46   F   | 102 66  f  | 
-	|	7  7   BEL |  39  27   '   | 71 47   G   | 103 67  g  |  
-	|	8  8   BS  |  40  28   (   | 72 48   H   | 104 68  h  |   
-	|	9  9   HT  |  41  29   )   | 73 49   I   | 105 69  i  |  
-	|	10 0a  LF  |  42  2a   *   | 74 4a   J   | 106 6a  j  |  
-	|	11 0b  VT  |  43  2b   +   | 75 4b   K   | 107 6b  k  |   
-	|	12 0c  FF  |  44  2c   ,   | 76 4c   L   | 108 6c  l  |   
-	|	13 0d  CR  |  45  2d   -   | 77 4d   M   | 109 6d  m  |    
-	|	14 0e  SO  |  46  2e   .   | 78 4e   N   | 110 6e  n  |    
-	|	15 0f  SI  |  47  2f   /   | 79 4f   O   | 111 6f  o  |   
-	|	16 10  DLE |  48  30   0   | 80 50   P   | 112 70  p  |   
-	|	17 11  DC1 |  49  31   1   | 81 51   Q   | 113 71  q  |   
-	|	18 12  DC2 |  50  32   2   | 82 52   R   | 114 72  r  |   
-	|	19 13  DC3 |  51  33   3   | 83 53   S   | 115 73  s  |   
-	|	20 14  DC4 |  52  34   4   | 84 54   T   | 116 74  t  |  
-	|	21 15  NAK |  53  35   5   | 85 55   U   | 117 75  u  |  
-	|	22 16  SYN |  54  36   6   | 86 56   V   | 118 76  v  |  
-	|	23 17  ETB |  55  37   7   | 87 57   W   | 119 77  w  | 
-	|	24 18  CAN |  56  38   8   | 88 58   X   | 120 78  x  |  
-	|	25 19  EM  |  57  39   9   | 89 59   Y   | 121 79  y  |      
-	|	26 1a  SUB |  58  3a   :   | 90 5a   Z   | 122 7a  z  |     
-	|	27 ab  ESC |  59  3b   ;   | 91 5b   [   | 123 7b  {  |        
-	|	28 1c  FS  |  60  3c   <   | 92 5c   \   | 124 7c  |  |      
-	|	29 1d  GS  |  61  3d   =   | 93 5d   ]   | 125 7d  }  |       
-	|	30 1e  RS  |  62  3f   >   | 94 5e   ^   | 126 7e  ~  |    
-	|	31 1f  US  |  63  3e   ?   | 95 5f   _   | 127 7f  DEL|     
-	|-------------------------------------------------------------|
-
+	
 KEY
 NULL:string terminator,EOT:end of transmission/terminates shell session,BELL:causes device to make noise,BS:backspace(erase last character),HT:horizontal tab(move cursor to right)LF:line feed(end-of-line marker),CR:carriage return(used in combination with LF as EOL for Windows),SUB:substitute char/end of file/ctrl-Z/suspend
 
