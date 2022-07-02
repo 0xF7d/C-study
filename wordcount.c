0960c266ed839a0042a3935b8f1774beaa26191a
crcirq32 <70669932+crcirq32@users.noreply.github.com>
2022-07-02 09:19:55 -0700

    Create wordcount.c

---
+++ wordcount.c
@@ -0,0 +1,26 @@
+/* cat <filename> | ./wordcount*/
+#include <stdio.h>
+
+#define IN 0 /* Inside a word */
+#define OUT 1 /* Outside a word */
+
+/* count lines, words, and characters in input */
+int main(){
+	int c, n1, nw, nc, state;
+	state = OUT;
+	n1 = nw = nc = 0;
+	while ((c = getchar()) != EOF)
+	{
+		++nc;
+		if (c == '\n')	
+			++n1;
+		if (c == ' ' || c == '\n' || c == '\t')	
+			state = OUT;
+		else if ( state == OUT)
+		{
+			state = IN;
+			++nw;
+		}
+	}
+	printf("%d %d %d\n" , n1, nw, nc);
+}
